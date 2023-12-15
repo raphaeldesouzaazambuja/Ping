6643e824e8241cb54ab259c06ed36692ed24f433
RAPHAEL DE SOUZA AZAMBUJA <raphael.azambuja@pmc.local>
2023-12-15 17:02:25 -0300

    ajustado css

--- index.php
+++ index.php
@@ -4,68 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Monitoramento de Relógios Pontos</title>
-    <style>
-
-        *{
-            padding: 0;
-            margin: 0;
-        }
-
-        body {
-            font-family: Arial, sans-serif;
-            margin: 20px;
-        }
-        h1 {
-            color: #333;
-            text-align: center;
-        }
-
-        form {
-            margin-bottom: 20px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-        }
-        
-        input{
-            width: 300px;
-            padding: 10px 5px;
-            outline: none;
-        }
-
-        button{
-            width: 300px;
-            padding: 10px 5px;
-            border: none;
-            background: #F0E7E9;
-            transition: border-radius 0.3s, color 0.3s; /* Adicione esta linha para a transição */
-        }
-
-        button:hover{
-            border-radius: 10px;
-            background: #D30001;
-
-        }
-
-        .result {
-            background-color: #f5f5f5;
-            padding: 10px;
-            border-radius: 5px;
-            margin-bottom: 10px;
-        }
-        .error {
-            color: #ff0000;
-        }
-    </style>
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
 
-<h1>Monitoramento das bolas</h1>
+<h1>Inspeção de bolas</h1>
 
 <form method="post" action="">
     <input type="text" id="bolinha" name="bolinha" placeholder="Digite o valor da bolinha:" required>
-    <br>
     <button type="submit">Realizar Monitoramento</button>
 </form>
 
@@ -84,11 +30,11 @@ function realizarPing($ip) {
     exec("ping " . $ip, $output, $status);
 
     echo '<div class="result">';
-    echo "<p>Disparando $ip com 32 bytes de dados:</p>";
+    echo "<p>Disparando $ip:</p>";
     
     if (!$status)
     {
-        echo "<pre>" . implode("\n", $output) . "</pre>";
+        echo "<pre>" .  $output . "</pre>";
     } 
     else 
     {
